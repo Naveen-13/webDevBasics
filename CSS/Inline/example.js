c0315056b43f52e69a999588547fff29c7ad3576
Naveen-13 <80417331+Naveen-13@users.noreply.github.com>
2025-12-01 15:49:25 +0530

    Add basic HTML structure in example.js

---
+++ CSS/Inline/example.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html style="background-color: blue;" lang="en">
+    <!-- css is at same line at html level -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
